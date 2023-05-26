57c2fa3efe7d2d9f944962d997bb484ef2c3b8f0
Tommicavo <tommicava@icloud.com>
2023-05-26 16:28:19 +0200

    final

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const age = document.getElementById("age");
 const price = document.getElementById("price");
 const sale = document.getElementById("sale");
 const priceSaled = document.getElementById("price_saled");
+const minAge = 18;
+const maxAge = 65;
 
 let isValid = true;
 let distErr = false;
@@ -13,13 +15,14 @@ let ageErr = false;
 let isSaled = false;
 let message = "Incorrect fields:\n";
 let mySale = 0;
-let myPriceSaled = 0;
+
 
 // userInput
 const myDistance = parseInt(prompt("Quanti km vuoi percorrere?", "100").trim());
 const myAge = parseInt(prompt("Quanti anni hai?", "20").trim());
 const myPrice = parseFloat((myDistance * tariffa).toFixed(2));
-// console.log(myDistance, myAge, myPrice);
+let myPriceSaled = myPrice;
+console.log(myDistance, myAge, myPrice, mySale, myPriceSaled);
 
 // validation
 if (isNaN(myDistance)){
@@ -35,7 +38,7 @@ if (distErr || ageErr){
     alert(message);
 }
 
-if (myAge < 18 || myAge > 65){
+if (myAge < minAge || myAge > maxAge){
     isSaled = true;
 }
 
@@ -45,16 +48,16 @@ if (isValid){
     price.innerText = myPrice;
 
     if (isSaled){
-        if (myAge < 18){
+        if (myAge < minAge){
             mySale = 0.2;
         } else {
             mySale = 0.4;
         }
         myPriceSaled = myPrice - (myPrice * mySale);
-        console.log("myPrice: " + myPrice);
-        console.log("mySale: " + mySale);
-        console.log("myPriceSaled: " + myPriceSaled);
-        sale.innerText = mySale;
-        priceSaled.innerText = myPriceSaled;
+        sale.innerText = "Sale: " + (mySale * 100) + "%";
+        priceSaled.innerText = "Price saled: " + myPriceSaled + "€";
     }
+    console.log("myPrice: " + myPrice);
+    console.log("mySale: " + mySale);
+    console.log("myPriceSaled: " + myPriceSaled);
 }
