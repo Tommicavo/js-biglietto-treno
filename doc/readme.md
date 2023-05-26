2557d62c63326baa5f8e76c24b21293e3c7d9cf7
Tommicavo <tommicava@icloud.com>
2023-05-26 15:15:40 +0200

    first commit

---
+++ doc/readme.md
@@ -0,0 +1,7 @@
+# Biglietto Treno
+
+- chiedere i km
+- chiedere l'età
+- verificare i dati
+- calcolare sconto
+- stampare info in pagina
---
+++ js/script.js
@@ -0,0 +1,56 @@
+console.log("js ok");
+
+const tariffa = 0.21
+const distance = document.getElementById("distance");
+const age = document.getElementById("age");
+const price = document.getElementById("price");
+const sale = document.getElementById("sale");
+const priceSaled = document.getElementById("price_saled");
+
+let isValid = true;
+let distErr = false;
+let ageErr = false;
+let isSaled = false;
+let message = "Incorrect fields:\n";
+let mySale = 0;
+
+// userInput
+const myDistance = parseInt(prompt("Quanti km vuoi percorrere?", "100").trim());
+const myAge = parseInt(prompt("Quanti anni hai?", "20").trim());
+const myPrice = parseFloat((myDistance * tariffa).toFixed(2));
+const myPriceSaled = myPrice - (myPrice * mySale);
+console.log(myDistance, myAge, myPrice);
+
+// validation
+if (isNaN(myDistance)){
+    distErr = true;
+    message += '"Distance"\n'
+}
+if (isNaN(myAge)){
+    ageErr = true;
+    message += '"Age"'
+}
+if (distErr || ageErr){
+    isValid = false;
+    alert(message);
+}
+
+if (myAge < 18 || myAge > 65){
+    isSaled = true;
+}
+
+if (isValid){
+    distance.innerText = myDistance;
+    age.innerText = myAge;
+    price.innerText = myPrice;
+
+    if (isSaled){
+        if (myAge < 18){
+            mySale = 0.2;
+        } else {
+            mySale = 0.4;
+        }
+        sale.innerText = mySale;
+        priceSaled.innerText = myPriceSaled;
+    }
+}
